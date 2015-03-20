1d871d8e4f1a84cf35576a04df3930da7837f0be
Mia Petkovic <mia@vet-cloud.net>
2015-03-20 13:13:23 +0100

    0.0.5 sport folder exapnded

--- bbc.py
+++ bbc.py
@@ -11,130 +11,9 @@ def scrape(topic, c = 0):
 	### TO DO ###
 	#Include reading from list of rss feeds and maintain acurate count number
 	#Read that number from the given filelist.txt
-	rss_list = ['http://rss.cnn.com/rss/edition_sport.rss',
-				'http://sports.yahoo.com/top/rss.xml',
-				'http://sports.yahoo.com/mlb/rss.xml',
-				'http://sports.yahoo.com/nfl/rss.xml',
-				'http://l.yimg.com/a/i/us/ext/rss4.gif',
-				'http://sports.yahoo.com/nhl/rss.xml',
-				'http://sports.yahoo.com/nascar/rss.xml',
-				'http://sports.yahoo.com/golf/rss.xml',
-				'http://sports.yahoo.com/mma/rss.xml',
-				'http://sports.yahoo.com/box/rss.xml',
-				'http://sports.yahoo.com/ncaab/rss.xml',
-				'http://sports.yahoo.com/ncaaw/rss.xml',
-				'http://sports.yahoo.com/ten/rss.xml',
-				'http://sports.yahoo.com/ncaabb/rss.xml',
-				'http://sports.yahoo.com/oly/rss.xml',
-				'http://sports.yahoo.com/irl/rss.xml',
-				'http://sports.yahoo.com/sow/rss.xml',
-				'http://sports.yahoo.com/mls/rss.xml',
-				'http://sports.yahoo.com/ski/rss.xml',
-				'http://sports.yahoo.com/sc/rss.xml',
-				'http://www.telegraph.co.uk/sport/othersports/boxing/rss',
-				'http://www.telegraph.co.uk/sport/olympics/badminton/rss',
-				'http://www.telegraph.co.uk/sport/football/teams/barcelona/rss',
-				'http://www.telegraph.co.uk/sport/football/teams/bosnia-and-herzegovina/rss'
-				'http://www.telegraph.co.uk/sport/football/teams/brazil/rss',
-				'http://www.telegraph.co.uk/sport/football/teams/serbia/rss',
-				'http://www.telegraph.co.uk/sport/olympics/table-tennis/rss',
-				'http://www.telegraph.co.uk/sport/motorsport/formulaone/usf1/rss',
-				'http://www.telegraph.co.uk/sport/olympics/water-polo/rss',
-				'http://www.telegraph.co.uk/sport/olympics/wrestling/rss',
-				'http://www.telegraph.co.uk/sport/olympics/rss',
-				'http://www.telegraph.co.uk/sport/tennis/novakdjokovic/rss',
-				'http://www.telegraph.co.uk/sport/olympics/news/rss',
-				'http://www.telegraph.co.uk/sport/football/competitions/premier-league/premier-league-clubs/rss',
-				'http://www.telegraph.co.uk/sport/othersports/polo/rss',
-				'http://www.telegraph.co.uk/sport/motorsport/rallying/rss',
-				'http://www.telegraph.co.uk/sport/football/teams/real-madrid/rss',
-				'http://www.telegraph.co.uk/sport/othersports/americanfootball/rss',
-				'http://www.telegraph.co.uk/sport/football/teams/arsenal/rss',
-				'http://www.telegraph.co.uk/sport/motorsport/formulaone/f1news/rss',
-				'http://www.telegraph.co.uk/sport/football/competitions/euro-2012/rss',
-				'http://www.telegraph.co.uk/sport/football/euro-2016/rss',
-				'http://www.telegraph.co.uk/sport/football/teams/denmark/rss',
-				'http://www.telegraph.co.uk/sport/olympics/cycling/rss',
-				'http://www.telegraph.co.uk/sport/cricket/rss',
-				'http://www.telegraph.co.uk/sport/olympics/football/rss',
-				'http://www.telegraph.co.uk/sport/football/teams/france/rss',
-				'http://www.telegraph.co.uk/sport/rugbyunion/international/france/rss',
-				'http://www.telegraph.co.uk/sport/motorsport/formulaone/rss',
-				'http://www.telegraph.co.uk/sport/football/footballfive/rss',
-				'http://www.telegraph.co.uk/sport/olympics/gymnastics/rss',
-				'http://www.telegraph.co.uk/sport/othersports/icehockey/rss',
-				'http://www.telegraph.co.uk/sport/olympics/judo/rss',
-				'http://www.telegraph.co.uk/sport/othersports/lacrosse/rss',
-				'http://www.telegraph.co.uk/sport/rugbyunion/international/newzealand/rss',
-				'http://www.telegraph.co.uk/sport/rugbyunion/international/newzealand/rss',
-				'http://www.telegraph.co.uk/sport/rugbyunion/news/rss',
-				'http://www.telegraph.co.uk/sport/olympics/sailing/rss',
-				'http://www.telegraph.co.uk/sport/football/teams/slovakia/rss',
-				'http://www.telegraph.co.uk/sport/football/teams/spain/rss',
-				'http://www.telegraph.co.uk/sport/sportvideo/sport-explained/rss',
-				'http://www.telegraph.co.uk/sport/sports-personality-of-the-year/rss',
-				'http://www.telegraph.co.uk/sport/football/teams/switzerland/rss',
-				'http://www.telegraph.co.uk/sport/othersports/cycling/tour-de-france/rss',
-				'http://www.telegraph.co.uk/sport/othersports/cycling/track-cycling/rss',
-				'http://www.telegraph.co.uk/sport/othersports/ufc/rss',
-				'http://www.telegraph.co.uk/sport/othersports/sailing/volvo-ocean-race/rss',
-				'http://www.telegraph.co.uk/sport/olympics/volleyball/rss',
-				'http://www.telegraph.co.uk/sport/tennis/wtatour/rss',
-				'http://www.telegraph.co.uk/sport/football/world-cup/rss',
-				'http://www.telegraph.co.uk/sport/tennis/williamssisters/rss',
-				'http://www.telegraph.co.uk/sport/olympics/weightlifting/rss',
-				'http://www.telegraph.co.uk/sport/golf/usopen/rss',
-				'http://www.telegraph.co.uk/sport/othersports/surfing/rss',
-				'http://www.telegraph.co.uk/sport/tennis/rafaelnadal/rss',
-				'http://www.telegraph.co.uk/sport/olympics/modern-pentathlon/rss',
-				'http://www.telegraph.co.uk/sport/othersports/athletics/london-marathon/rss',
-				'http://www.telegraph.co.uk/sport/football/players/landon-donovan/rss',
-				'http://www.telegraph.co.uk/sport/football/teams/manchester-city/rss',
-				'http://www.telegraph.co.uk/sport/football/teams/mexico/rss',
-				'http://www.telegraph.co.uk/sport/mysport/rss',
-				'http://www.telegraph.co.uk/sport/columnists/matt-law/rss',
-				'http://www.telegraph.co.uk/sport/olympics/mobile-guides/rss',
-				'http://www.telegraph.co.uk/sport/football/teams/manchester-united/rss',
-				'http://preview.telegraph.co.uk/sport/columnists/james-anderson/rss',
-				'http://www.telegraph.co.uk/sport/columnists/jacquelin-magnay/rss',
-				'http://www.telegraph.co.uk/sport/football/european/rss',
-				'http://www.telegraph.co.uk/sport/golf/europeantour/rss',
-				'http://www.telegraph.co.uk/sport/football/teams/exeter-city/rss',
-				'http://www.telegraph.co.uk/sport/rugbyunion/european-rugby/rss',
-				'http://www.telegraph.co.uk/sport/tennis/daviscup/rss',
-				'http://www.telegraph.co.uk/sport/cricket/cricket-world-cup/rss'
-				'http://feeds.reuters.com/reuters/sportsNews',
-				'http://feeds.bbci.co.uk/sport/0/football/rss.xml?edition=uk',
-				'http://feeds.bbci.co.uk/sport/0/tennis/rss.xml?edition=uk',
-				'http://feeds.bbci.co.uk/sport/0/cricket/rss.xml?edition=uk',
-				'http://feeds.bbci.co.uk/sport/0/rugby-union/rss.xml?edition=uk',
-				'http://feeds.bbci.co.uk/sport/0/rugby-league/rss.xml?edition=uk',
-				'http://newsrss.bbc.co.uk/rss/sportplayer_uk_edition/golf/rss.xml',
-				'http://newsrss.bbc.co.uk/rss/sportplayer_uk_edition/motorsport/rss.xml',
-				'http://newsrss.bbc.co.uk/rss/sportplayer_uk_edition/boxing/rss.xml',
-				'http://newsrss.bbc.co.uk/rss/sportplayer_uk_edition/athletics/rss.xml',
-				'http://newsrss.bbc.co.uk/rss/sportplayer_uk_edition/snooker/rss.xml',
-				'http://www.huffingtonpost.com/feeds/verticals/sports/index.xml',
-				'http://www.huffingtonpost.com/news/combat-sports/feed/',
-				'http://www.huffingtonpost.com/news/detroit-sports/feed/',
-				'http://www.huffingtonpost.com/news/fox-sports-1/feed/',
-				'http://www.huffingtonpost.com/news/religion-and-sports/feed/',
-				'http://www.huffingtonpost.com/news/impact-sports/feed/',
-				'http://www.huffingtonpost.com/news/miami-sports/feed/'
-				'http://www.huffingtonpost.com/news/sports-fails/feed/',
-				'http://www.huffingtonpost.com/news/science-of-sport/feed/',
-				'http://www.huffingtonpost.com/news/motorsports/feed/',
-				'http://www.huffingtonpost.com/news/basketball/feed/',
-				'http://www.huffingtonpost.com/news/big-ten-football/feed/'
-				'http://www.huffingtonpost.com/news/baseball/feed/',
-				'http://www.huffingtonpost.com/news/football/feed/',
-				'http://www.huffingtonpost.com/news/football-foods/feed/',
-				'http://www.huffingtonpost.com/news/olympic-basketball/feed/',
-				'http://www.huffingtonpost.com/news/olympic-gymnastics/feed/',
-				'http://www.huffingtonpost.com/news/olympic-glory/feed/',
-				'http://www.huffingtonpost.com/news/olympic-soccer/feed/',
-				'http://www.huffingtonpost.com/news/olympics/feed/',
-				'http://www.huffingtonpost.com/news/tennis/feed/'
+	rss_list = ['http://www.huffingtonpost.com/news/nba-draft-2012/feed/',
+				'http://www.huffingtonpost.com/news/2012-nba-playoffs/feed/',
+				'http://www.huffingtonpost.com/news/nfl-draft-2012/feed/'
 				]
 			
 	for rss_url in rss_list:
@@ -168,20 +47,8 @@ def scrape(topic, c = 0):
 							# ToDo write switch if url contains washingtonpost than p_list ...
 							# default: most used case with
 
-							# For Waashington post 			p_list = soup.find_all('article')
-							#'''For zdnet, bbc, techrepublic
-
-							#p_list = soup.find("div", {"class": ["article", 'emp-decription',
-							#'story-body','content','l-container', 'storyBody', 'column1','p402_premium','body', 'story', 
-							#'zn-body__paragraph', 'cnnContentContainer']}).find_all('p')
-
-							#p_list = soup.find("div", {"id": ["mainBodyArea"]}).find_all('p')
-							# print p_list
-							# break
-
-							#For CNN
 							if link.find('cnn') != -1:
-								p_list = soup.find_all("p", {'class': ['zn-body__paragraph', 'cnnContentContainer']}) #CNN
+							    p_list = soup.find_all("p", {'class': ['zn-body__paragraph', 'cnnContentContainer']}) #CNN
 							elif link.find('washingtonpost') != -1:
 								p_list = soup.find_all('article')
 							elif link.find('cbc') != -1:
@@ -192,10 +59,6 @@ def scrape(topic, c = 0):
 								p_list = soup.find('article', {'class' : ['article_cbox']}).find_all('p')
 							elif link.find('huffingtonpost') != -1:
 								p_list = soup.find('div', {'id': 'mainentrycontent'}).find_all('p')
-
-							# elif link.find('nationaljournal') != -1:
-							# 	p_list = soup.find('id', {'class': ['articleBodyContent']})
-							
 							else:
 								p_list = soup.find('div', {'class': ['magWYSIWYG','WYSIWYG', 'article','emp-decription',
 								'story-body','content','l-container', 'storyBody', 'column1','p402_premium','body', 'story', 
@@ -254,4 +117,4 @@ def remove_new_line(text):
 	return ' '.join(text.split())
 
 
-scrape('sport',0)
+scrape('sport',1966)
