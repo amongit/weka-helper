19f512de21e7d811137fa19714d152fa4d3cf226
Mia Petkovic <mia@vet-cloud.net>
2015-03-24 13:06:34 +0100

    0.0.9 nltk parser updated

--- nltk_parser.py
+++ nltk_parser.py
@@ -1,20 +1,28 @@
 import nltk
 from nltk import pos_tag, word_tokenize
+
 with open('politics_train_set.txt', 'r') as myfile: 
 	politics = [line.decode('utf-8').strip() for line in myfile.readlines()]
-	print politics
 	myfile.close()
-	#politics_list = politics.split('\n')
 
-for line in politics:
+text = []
+tagged_text = []
+for i, line in enumerate(politics):
 	with open(line, 'r') as myfile: 
 		whole_text = myfile.read().decode('utf-8')
 		myfile.close()
+	text_tokenized = word_tokenize(whole_text)
+	text_pos_tagged = nltk.pos_tag(text_tokenized)
+	tag_fd = nltk.FreqDist(tag for (word, tag) in text_pos_tagged)
+	''' najcesca vrsta reci pre imenice 
+	word_tag_pairs = nltk.bigrams(text_pos_tagged)
+	noun_preceders = [a[1] for (a, b) in word_tag_pairs if b[1] == 'NN'] 
+	fdist = nltk.FreqDist(noun_preceders)
+	print [tag for (tag, _) in fdist.most_common()]
+	'''
 
-	# text = nltk.Text(word.lower() for word in whole_text)
-	text = word_tokenize(whole_text)
-
-	text_tagged = nltk.pos_tag(text)
-	#print text_tagged
-	tag_fd = nltk.FreqDist(tag for (word, tag) in text_tagged)
-	print tag_fd.most_common()
+	'''' glagoli poredjani po ucestalosti u tekstu'''
+	[wt[0] for (wt, _) in tag_fd.most_common() if wt[1] == 'VBD']
+	#vraca najcestiji pos
+	#print tag_fd.most_common()[0][1]
+	break
